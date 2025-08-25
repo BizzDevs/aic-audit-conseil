9bf9728b3a0ae3a37374eaebaf4a0742f65fde8b
Essebe Ngwa <essebe.ngwa@gmail.com>
2025-08-25 01:06:18 +0100

    minified javascript files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,66 +31,6 @@ jQuery( document ).ready(function( $ ) {
             $("header").removeClass("background-header");
           }
         });
-		if ($('.owl-testimonials').length) {
-            $('.owl-testimonials').owlCarousel({
-                loop: true,
-                nav: false,
-                dots: true,
-                items: 1,
-                margin: 30,
-                autoplay: false,
-                smartSpeed: 700,
-                autoplayTimeout: 6000,
-                responsive: {
-                    0: {
-                        items: 1,
-                        margin: 0
-                    },
-                    460: {
-                        items: 1,
-                        margin: 0
-                    },
-                    576: {
-                        items: 2,
-                        margin: 20
-                    },
-                    992: {
-                        items: 2,
-                        margin: 30
-                    }
-                }
-            });
-        }
-        if ($('.owl-partners').length) {
-            $('.owl-partners').owlCarousel({
-                loop: true,
-                nav: false,
-                dots: true,
-                items: 1,
-                margin: 30,
-                autoplay: false,
-                smartSpeed: 700,
-                autoplayTimeout: 6000,
-                responsive: {
-                    0: {
-                        items: 1,
-                        margin: 0
-                    },
-                    460: {
-                        items: 1,
-                        margin: 0
-                    },
-                    576: {
-                        items: 2,
-                        margin: 20
-                    },
-                    992: {
-                        items: 4,
-                        margin: 30
-                    }
-                }
-            });
-        }
 
         $(".Modern-Slider").slick({
             autoplay:true,
@@ -121,25 +61,5 @@ jQuery( document ).ready(function( $ ) {
             return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
 
         }
-
-        $(window).scroll(function(){
-
-          if(visible($('.count-digit')))
-            {
-              if($('.count-digit').hasClass('counter-loaded')) return;
-              $('.count-digit').addClass('counter-loaded');
-              
-        $('.count-digit').each(function () {
-          var $this = $(this);
-          jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-            duration: 3000,
-            easing: 'swing',
-            step: function () {
-              $this.text(Math.ceil(this.Counter));
-            }
-          });
-        });
-        }
-    })
  
 });
